0ff5debc29624839dd824ed00aee84b9460c0a83
calee14 <caplee1000@gmail.com>
2020-07-11 15:38:54 -0700

    testing add

--- opportunity/views.py
+++ opportunity/views.py
@@ -1,7 +1,7 @@
 # Imports
 from opportunity import app, login_manager
+from opportunity.dbmodels.oppmodel import Opportunity
 from flask import render_template, make_response, url_for, send_file, abort
-
 '''
 Views
 '''
@@ -13,4 +13,7 @@ def load_user(id):
 
 @app.route('/', methods=['GET'])
 def index():
+    opp = Opportunity()
+    opp.add("opp_title", "img", "desc", "link", "topic", "author")
+
     return render_template('home.html')
