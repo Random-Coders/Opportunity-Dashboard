df8c3ff34c5068dbf702edaa7c0b7461bfad36e9
calee14 <caplee1000@gmail.com>
2020-07-11 14:54:50 -0700

    adding func for adding opp

--- opportunity/dbmodels/oppmodel.py
+++ opportunity/dbmodels/oppmodel.py
@@ -5,4 +5,20 @@ class Opportunity(object):
 	def __init__(self,):
 
 		self.db = connect("opportunity")
-		
+
+	def add(opp_title, img, desc, link, topic, author):
+		opp_obj = { 
+			"title": opp_title,
+			"img": img,
+			"desc": desc,
+			"link": link,
+			"topic": topic,
+			"author": author
+		}
+
+		result = posts.insert_one(post_data)
+		# for now 
+		if result.inserted_id:
+			return True
+		else:
+			return False
