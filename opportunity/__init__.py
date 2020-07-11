1974e01d2fbb2f55bae4665e666e85d8eb892693
RafaelCenzano <savagecoder77@gmail.com>
2020-07-11 11:45:42 -0700

    create init file

---
+++ opportunity/__init__.py
@@ -0,0 +1,16 @@
+# Import Flask for flask app object
+from flask import Flask
+from config import Config
+
+
+# Create flask app object
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Create Login manager
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'warning'
+
+# Import all views
+import opportunity.views
