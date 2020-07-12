58bd4e69379f77be5c965dd47dc1bd6c64fd4842
RafaelCenzano <savagecoder77@gmail.com>
2020-07-11 23:39:01 -0700

    update register form

--- opportunity/forms.py
+++ opportunity/forms.py
@@ -19,11 +19,10 @@ class LoginForm(FlaskForm):
 
 class RegisterUser(FlaskForm):
     email = EmailField('Email address', [DataRequired(), Email()])
-    title = StringField('Title', validators=[DataRequired()], render_kw={"placeholder": "Enter your name or organization"})
+    title = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Enter your name or organization"})
     password = PasswordField(
         'Password',
         validators=[
-            Length(min=2),
             DataRequired(),
             EqualTo('confirm', message='Passwords must match')
         ])
