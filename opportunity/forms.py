c2db217ad59dce0b6a4e6b7c73aafda9d849b22f
RafaelCenzano <savagecoder77@gmail.com>
2020-07-11 23:20:38 -0700

    update forms

--- opportunity/forms.py
+++ opportunity/forms.py
@@ -16,53 +16,17 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-'''
-class RequestResetForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email(), Length(max=120)])
-    submit = SubmitField('Request Password Reset')
-
-
-class ResetPasswordForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email(), Length(max=120)])
-    password = PasswordField(
-        'Password',
-        validators=[
-            DataRequired(),
-            Length(
-                min=6,
-                max=150,
-                message='Password must be within 6 and 150 characters')])
-    confirm_password = PasswordField(
-        'Confirm Password', validators=[
-            DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset Password')
-'''
-
 
 class RegisterUser(FlaskForm):
     email = EmailField('Email address', [DataRequired(), Email()])
     title = StringField('Title', validators=[DataRequired()], render_kw={"placeholder": "Enter your name or organization"})
     password = PasswordField(
         'Password',
-        [
+        validators=[
             Length(min=2),
             DataRequired(),
             EqualTo('confirm', message='Passwords must match')
         ])
     confirm = PasswordField('Repeat password')
     submit = SubmitField('Create Account')
-
-
-class CreatePost(FlaskForm):
-    title = StringField(
-        'Title', validators=[
-            DataRequired(), Length(
-                max=30, message='First name must be 30 characters or less')])
-    description = TextAreaField(
-        'Description', widget=[TextArea],
-            validators=[
-            DataRequired(), Length(
-                max=30, message='First name must be 30 characters or less')])
 
