1ee744d0179d18b446da6103b261c26f9169df9d
calee14 <caplee1000@gmail.com>
2020-07-11 21:43:26 -0700

    delete old class

--- opportunity/dbmodels/usermodel.py
+++ opportunity/dbmodels/usermodel.py
@@ -3,44 +3,6 @@ from flask_login import UserMixin, login_user
 from opportunity import login_manager, bcrypt
 import uuid
 
-# class User:
-
-#     def __init__(self, email, title, password):
-
-#         self.db = connect("users")
-
-#         self.email = email
-#         self.title = title
-#         self.password = password
-
-#         self.is_authenticated = False
-#         self.is_active = False
-#         self.is_anonymous = False
-        
-
-#     def store(self):
-#         user_obj = { 
-#             "email": self.email,
-#             "title": self.title,
-#             "password": self.password
-#         }
-#         # start a collection
-#         users_collection = self.db.users
-
-#         result = users_collection.insert_one(user_obj)
-#         # for now return status of db insert
-#         if result.inserted_id:
-#             return True
-#         else:
-#             return False
-
-#     def get_id(self):
-#         query = {"email":self.email}
-#         user = self.db.users.find(query)[0]
-#         return user['_id']
-
-#     def __repr__(self):
-#         return '<User {}>'.format(self.email)
 users_db = connect('users').users
 
 class User(UserMixin):
